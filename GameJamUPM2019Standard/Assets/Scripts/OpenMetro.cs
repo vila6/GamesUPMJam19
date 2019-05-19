3739a2ca3c3776e37752cb8ffe8efcd2e768c821
vila6 <mrvila@esei.uvigo.es>
2019-05-19 16:51:21 +0200

    Metro sounds

--- GameJamUPM2019Standard/Assets/Scripts/OpenMetro.cs
+++ GameJamUPM2019Standard/Assets/Scripts/OpenMetro.cs
@@ -7,5 +7,6 @@ public class OpenMetro : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         GetComponent<Animator>().SetTrigger("Open");
+        GetComponent<AudioSource>().Play();
     }
 }

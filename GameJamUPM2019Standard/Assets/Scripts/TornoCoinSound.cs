b3b5d1204cce6433538b71de4295e7c1bc802728
Xavicity <39417469+Xavicity@users.noreply.github.com>
2019-05-19 13:04:19 +0200

    Metro Vallecats Ida donette

---
+++ GameJamUPM2019Standard/Assets/Scripts/TornoCoinSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornoCoinSound : MonoBehaviour
+{
+    public AudioClip coinSound;
+    private bool hasSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!hasSound)
+        {
+            hasSound=true;
+            this.GetComponent<AudioSource>().PlayOneShot(coinSound);
+        }
+        
+    }
+}

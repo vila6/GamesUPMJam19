0beaf6a24aaafb2c62faeb2517144542859fab56
vila6 <mrvila@esei.uvigo.es>
2019-05-19 14:26:51 +0200

    Audios y tal

---
+++ GameJamUPM2019Standard/Assets/Scripts/StepsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepsController : MonoBehaviour
+{
+    public AudioClip [] stepAudios;
+
+    public void PlayRandomStep()
+    {
+        GetComponent<AudioSource>().clip = stepAudios[Random.Range(0, stepAudios.Length)];
+        GetComponent<AudioSource>().Play();
+        Debug.Log("Playing step");
+    }
+}
